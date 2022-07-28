52b6778d40f8bf5b26b8b0604b5f1adf9136d104
Maiterivas <maiterivas09@gmail.com>
2022-07-28 15:21:21 -0500

    final queries upload

--- EmployeeSQL/Employee_Data_Query.sql
+++ EmployeeSQL/Employee_Data_Query.sql
@@ -56,3 +56,7 @@ ORDER BY emp_no;
 
 --8. List the frequency count of employee last names 
 --   (i.e., how many employees share each last name) in descending order.
+SELECT last_name, COUNT(last_name) AS "last_name_count"
+FROM employees
+GROUP BY last_name
+ORDER BY "last_name_count" DESC;
